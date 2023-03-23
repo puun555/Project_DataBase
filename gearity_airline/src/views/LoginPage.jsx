e20ab97c5a1b4ee27cea580e044e3bc2c6c9e9cd
Geardevop <gearkiskeedev@gmail.com>
2023-03-23 20:57:00 +0700

    set up

---
+++ gearity_airline/src/views/LoginPage.jsx
@@ -0,0 +1,13 @@
+import * as React from 'react';
+import Container from '@mui/material/Container';
+import Box from '@mui/material/Box';
+const LoginPage = () =>{
+    return(
+        <>
+           
+        </>
+    )
+}
+
+export default LoginPage
+
