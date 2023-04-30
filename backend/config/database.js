03fe399eb180a6f6e25e2e6aa26ee9d573d37d51
PuunKotHodJumpFuc <105053170+puun555@users.noreply.github.com>
2023-05-01 00:28:01 +0700

    xxxx

--- backend/config/database.js
+++ backend/config/database.js
@@ -4,7 +4,16 @@ const connection = mysql.createConnection({
     host : 'localhost',
     user : 'root',
     password : '123456',
-    database: 'test'
+    database: 'new_schema'
 });
 
+connection.connect((err)=>{
+    if(err) throw err;
+    console.log('connected to database')
+}
+);
+
+//create Routes
+
 module.exports = connection.promise();
+
--- backend/index.js
+++ backend/index.js
@@ -1,14 +1,59 @@
 const exprees = require('express')
 const app =  exprees()
-const route = require('./route/name')
+const mysql = require('mysql2')
 
-const port = 3000
+const port = 5500
 
-app.get("/", (req,res)=>{
-    res.send('gearaline ja')
+app.use(exprees.json());
+
+const connection = mysql.createConnection({
+    host : 'localhost',
+    user : 'root',
+    password : '123456',
+    database: 'test'
+});
+
+connection.connect((err)=>{
+    if(err) throw err;
+    console.log('connected to database')
+});
+
+app.get('/read', async (req,res)=>{
+    const {id, firstName, lastName} = req.body
+    try {
+        connection.query(`SELECT * FROM project_d.airport;`,
+        [id, firstName, lastName], 
+        (err, result, field)=>{
+            if(err) {
+                console.log(err); 
+                return res.status(400).send();
+            }
+            return res.status(200).send(result);
+        })
+    } catch (error) {
+        console.log(error)
+        return res.status(500).send('server error')
+    }
 })
 
-app.use('/database', route)
+app.post('/create', async (req,res)=>{
+    const {id, firstName, lastName} = req.body
+    try {
+        connection.query(`insert into new_table (id, firstName, lastName) values (?,?,?)`,
+        [id, firstName, lastName], 
+        (err, result, field)=>{
+            if(err) {
+                console.log(err); 
+                return res.status(400).send();
+            }
+            return res.status(200).json({message: 'success create'});
+        })
+    } catch (error) {
+        console.log(error)
+        return res.status(500).send('server error')
+    }
+})
+
 
 app.listen(port, ()=>{
     console.log('listening on port', port)
